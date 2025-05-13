8d10bafaf9d046b3ba23f613284696e91de2b5c6
ProgGitEstacio <adelmo.bittencourt@hotmail.com>
2025-05-13 04:22:17 +0000

    nova versão

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,13 +5,13 @@ int main() {
     
     // Definindo as variáveis separadas para cada atributo da cidade 1;
 
-    int populacao2, npontosturisticos2;
-    float area2, PIB2;
-    char estado2[20], codigodacarta2[20], nomedacidade2[20];
     int populacao1, npontosturisticos1;
     float area1, PIB1;
     char estado1[20], codigodacarta1[20], nomedacidade1[20];
-      
+    int populacao2, npontosturisticos2;
+    float area2, PIB2;
+    char estado2[20], codigodacarta2[20], nomedacidade2[20];
+    
     // Cadastrando as Cartas, utilizando a função scanf para a captura das entradas do usuário para cada atributo da carta;
     printf("Digite o nome do Estado: \n");
     scanf("%s", &estado1);
@@ -28,16 +28,12 @@ int main() {
     printf("Digite a área da cidade: \n");
     scanf("%f", &area1);
 
-    printf("Digite o PIB da cidade: \n ");
+    printf("Digite o PIB da cidade: \n");
     scanf("%f", &PIB1);
 
-    printf(" Digite o número de pontos Turísticos: \n");
+    printf("Digite o número de pontos Turísticos: \n");
     scanf("%d", npontosturisticos1);
-        
-   // Definindo as variáveis separadas para cada atributo da cidade 2;
-
-      
-    // Cadastrando as Cartas, utilizando a função scanf para a captura das entradas do usuário para cada atributo da carta;
+    
     printf("Digite o nome do Estado: \n");
     scanf("%s", &estado2);
 
@@ -53,13 +49,13 @@ int main() {
     printf("Digite a área da cidade: \n");
     scanf("%f", &area2);
 
-    printf("Digite o PIB da cidade: \n ");
+    printf("Digite o PIB da cidade: \n");
     scanf("%f", &PIB2);
 
-    printf(" Digite o número de pontos Turísticos: \n");
+    printf("Digite o número de pontos Turísticos: \n");
     scanf("%d", npontosturisticos2);
     
-     // Exibindo os dos Dados das Cartas, um por linha,  utilizando a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
+    // Exibindo os dos Dados das Cartas, um por linha,  utilizando a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     printf("Carta 01:\n");
 
     printf("Estado:%s\n", estado1);
